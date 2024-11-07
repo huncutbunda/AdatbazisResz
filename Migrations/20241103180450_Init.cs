abcf0298d0f33b02dc4fac08351fa573b886b7a4
Szabó Ákos Csaba <szabo.akos.csaba@blathy.info>
2024-11-07 07:56:16 +0100

    Add project files.

---
+++ Migrations/20241103180450_Init.cs
@@ -0,0 +1,99 @@
+﻿/*
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdatbazisResz.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Persons_Persons_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentCards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Validity = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentCards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentTeacher",
+                columns: table => new
+                {
+                    StudentsId = table.Column<int>(type: "int", nullable: false),
+                    TeachersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentTeacher", x => new { x.StudentsId, x.TeachersId });
+                    table.ForeignKey(
+                        name: "FK_StudentTeacher_Persons_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Persons",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_StudentTeacher_Persons_TeachersId",
+                        column: x => x.TeachersId,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_ParentId",
+                table: "Persons",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentTeacher_TeachersId",
+                table: "StudentTeacher",
+                column: "TeachersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentCards");
+
+            migrationBuilder.DropTable(
+                name: "StudentTeacher");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
+*/
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using Microsoft.EntityFrameworkCore;
+
+public class DrawingDbContext : DbContext
+{
+    public DbSet<Drawing> Drawings { get; set; }
+    public DbSet<Point> Points { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AdatbazisResz;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Drawing>()
+            .HasMany(d => d.Points)
+            .WithOne(p => p.Drawing)
+            .HasForeignKey(p => p.DrawingId);
+            
+    }
+}
+public class Drawing
+{
+    [Key]
+    public int DrawingId { get; set; }  
+
+    [Required]
+    public string Name { get; set; }  
+
+    public DateTime CreatedDate { get; set; } = DateTime.Now;  
+
+    public ICollection<Point> Points { get; set; } = new List<Point>();
+}
+
+
+public class Point
+{
+    [Key]
+    public int PointId { get; set; }  
+
+    public int X { get; set; }  
+
+    public int Y { get; set; }  
+
+    public int DrawingId { get; set; }
+
+    [ForeignKey("DrawingId")]
+    public Drawing Drawing { get; set; }  
+}
